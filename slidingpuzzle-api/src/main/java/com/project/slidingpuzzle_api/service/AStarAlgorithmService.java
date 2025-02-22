7573ceff0317787f342bb59648d4a5fa21e6bf4a
Avijeet Nayak <avijeetnayak001@gmail.com>
2025-02-23 01:23:14 +0530

    Add A star algorithm service

---
+++ slidingpuzzle-api/src/main/java/com/project/slidingpuzzle_api/service/AStarAlgorithmService.java
@@ -0,0 +1,78 @@
+package com.project.slidingpuzzle_api.service;
+
+import com.project.slidingpuzzle_api.dto.TransitionStateDTO;
+import com.project.slidingpuzzle_api.model.MovementConstants;
+import com.project.slidingpuzzle_api.model.Puzzle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+@Service
+public class AStarAlgorithmService {
+
+    @Autowired
+    MovementService movementService;
+
+    private final String[] MOVEMENT_DECIDER = {MovementConstants.UP, MovementConstants.RIGHT, MovementConstants.DOWN, MovementConstants.LEFT};
+
+    public List<TransitionStateDTO> getShortestPathSolution(Puzzle puzzle) {
+        // declare a priority queue here
+        PriorityQueue<Puzzle> pq = new PriorityQueue<>();
+        pq.add(puzzle);
+        // at the end we will be having our solution object
+        // from this solution object, we trace down all the
+        Puzzle targetPuzzleState = null;
+
+        while (!pq.isEmpty()) {
+            Puzzle currState = pq.poll();
+            if (currState.isRelaxed()) {
+                continue;
+            }
+            currState.setRelaxed(true);
+            if (currState.getHeuristicValue() == 0) {
+                targetPuzzleState = new Puzzle(currState);
+                break;
+            }
+            // now for each of its adjacent, add it to the
+            for (int dir = 0; dir < 4; dir++) {
+                Puzzle adjacentState = new Puzzle(currState);
+                if (movementService.getToTheNextState(adjacentState, dir, -1)) {
+                    // Puzzle transformed to adjacent stage
+                    adjacentState.setParentPuzzle(currState);
+                    adjacentState.setMoveType(MOVEMENT_DECIDER[dir]);
+                    // all other fields got updated in the movement service
+                    pq.add(adjacentState);
+                }
+            }
+        }
+
+        // now we need to trace back the steps from the final State
+
+        return getListOfTransitionDetails(targetPuzzleState);
+    }
+
+    private List<TransitionStateDTO> getListOfTransitionDetails(Puzzle targetPuzzleState) {
+        List<TransitionStateDTO> results = new ArrayList<>();
+        while (null != targetPuzzleState.getParentPuzzle()) {
+            TransitionStateDTO currentTransition = new TransitionStateDTO();
+            currentTransition.setMoveType(targetPuzzleState.getMoveType());
+            currentTransition.setTargetGrid(targetPuzzleState.getGrid());
+            currentTransition.setSourceGrid(targetPuzzleState.getParentPuzzle().getGrid());
+
+            // add this to the result
+            results.add(currentTransition);
+
+            // trace back to the parent of this current state
+            targetPuzzleState = targetPuzzleState.getParentPuzzle();
+        }
+
+        // reverse the list, because we need from source to target
+        Collections.reverse(results);
+
+        return results;
+    }
+}
